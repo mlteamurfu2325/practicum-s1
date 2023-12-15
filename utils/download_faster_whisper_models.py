da8b8ea87b70bf2498a17b11e8a97f8645199ea7
khit-mle <149804920+khit-mle@users.noreply.github.com>
2023-12-15 22:32:50 +0500

    feat(utils): add faster-whisper models downloader
    Create utils/
    Create download_faster_whisper_models.py

---
+++ utils/download_faster_whisper_models.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from faster_whisper import download_model
+
+DIR = str(Path.cwd())+'/models/'
+MODELS = ['medium', 'large-v3']
+
+for model in MODELS:
+    try:
+        download_model(model, output_dir=f'{DIR}/{model}/')
+    except Exception as error:
+        print(f'An exception occured: {error}')
+
+print('Done!')
+
