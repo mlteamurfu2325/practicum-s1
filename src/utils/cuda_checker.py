6b9824ee4512454381589e413c8597321d5f214c
khit-mle <khit.mleng@gmail.com>
2023-12-16 18:31:10 +0300

    chore: add src/utils dir and add code for checking cude avail

---
+++ src/utils/cuda_checker.py
@@ -0,0 +1,16 @@
+import ctypes
+
+def check_cuda():
+    """
+    Check if CUDA GPUs are available without importing torch or tensorflow
+
+    Returns
+    -------
+    bool
+        True if a CUDA GPU is available, False otherwise
+    """
+    try:
+        ctypes.cdll.LoadLibrary("libcuda.so")
+        return True
+    except OSError:
+        return False
