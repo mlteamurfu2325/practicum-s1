a1a1f9748b709508aec2ff8371f1c492be0329b2
user1 <ikhardin000@gmail.com>
2023-12-20 22:35:01 +0500

    chore: fix bug valueError in summ_fetcher.py

--- src/llm_summ/summ_fetcher.py
+++ src/llm_summ/summ_fetcher.py
@@ -22,7 +22,6 @@ def fetch_summary(text: str,
     >>> summary = fetch_summary(api_key, text)
     
     """
-
     try:
         if os.environ.get('LLM_API_KEY', None):
             openai.api_key = os.environ['LLM_API_KEY']
