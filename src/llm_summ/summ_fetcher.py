7a00e8add6c0da8c465b1116b0a5f5eedf04c453
khit-mle <149804920+khit-mle@users.noreply.github.com>
2023-12-20 15:44:24 +0500

    chore(llm-summ): refactor summ_fetcher.py
    Fixes GH-11

--- src/llm_summ/summ_fetcher.py
+++ src/llm_summ/summ_fetcher.py
@@ -1,9 +1,39 @@
+import os
 import openai
 
 
-def fetch_summary(key: str, text: str) -> str:
-    openai.api_key = key
-    openai.base_url = "https://api.vsegpt.ru:6070/v1/"
+def fetch_summary(text: str,
+                  llm_model: str = 'openai/gpt-3.5-turbo-1106',
+                  llm_api_key: str | None = None) -> str:
+    """Fetch summary from LLM API using provided text and optionally an API key.
+    
+    :param text: Input text to summarize
+    :type text: str
+    :param llm_model: Name of LLM model to use
+    :type llm_model: str
+    :param llm_api_key: API key for service (optional)
+    :type llm_api_key: str, optional
+    :return: Summary text response from API 
+    :rtype: str
+    
+    :Example:
+    
+    >>> summary = fetch_summary(text)
+    >>> summary = fetch_summary(api_key, text)
+    
+    """
+    try:
+        if os.environ['LLM_API_KEY']:
+            openai.api_key = os.environ['LLM_API_KEY']
+        else:
+            openai.api_key = llm_api_key
+    except Exception as error:
+        raise ValueError(f"LLM_API key error: {error}")
+
+    try:
+        openai.base_url = os.environ['LLM_URL']
+    except Exception as error:
+        raise ValueError(f"LLM_URL error: {error}")
 
     prompt = f"Дай краткий пересказ этого текста: {text}"
 
@@ -12,7 +42,7 @@ def fetch_summary(key: str, text: str) -> str:
     messages.append({"role": "user", "content": prompt})
 
     response_big = openai.chat.completions.create(
-        model="fireworks/mixtral-8x7b-fw-chat",
+        model=llm_model,
         messages=messages,
         temperature=0.7,
         n=1,
