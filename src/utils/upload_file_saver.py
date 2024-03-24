31e512ac1eda5201cdb0d0afad72330062de80ff
LenaIkra <emikrina@gmail.com>
2024-03-24 19:06:36 +0000

    chore: code cleaning in upload_file_saver.py

--- src/utils/upload_file_saver.py
+++ src/utils/upload_file_saver.py
@@ -5,24 +5,21 @@ from pathlib import Path
 import streamlit as st
 
 
-# No type hints yet available for Streamlit
-# so no specifi type hint available for `uploaded_file`
-# See https://github.com/streamlit/streamlit/issues/7801
-def save_uploaded_file(uploaded_file) -> Path:
+def save_uploaded_file(uploaded_file: st.uploaded_file_manager.UploadedFile) -> Path:
     """
     Save an uploaded file to the specified directory and return the file path.
 
-    :param uploaded_file: The file uploaded by the user through the Streamlit interface.
-    :return: The path to the saved file.
-    :rtype: Path
+    Args:
+        uploaded_file: The file uploaded by the user through the Streamlit interface.
+
+    Returns:
+        Path: The path to the saved file.
     """
     dir_path = Path("../media")
     dir_path.mkdir(parents=True, exist_ok=True)
 
-    # create a path object for the file
     file_path = dir_path / uploaded_file.name
 
-    # write the file
     with file_path.open("wb") as f:
         f.write(uploaded_file.getbuffer())
 
