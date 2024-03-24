b849688476c532d29195488afe662d95c62e987d
ANGorbachev <a.n.gorba4ev@gmail.com>
2024-03-24 18:45:58 +0000

    refactor: code cleaning in cuda_checker.py and data_validator.py

--- src/utils/cuda_checker.py
+++ src/utils/cuda_checker.py
@@ -1,16 +1,14 @@
-"""Module to provide function to check if we can use a CUDA GPU."""
+"""Module to provide a function to check if a CUDA GPU is available."""
 
 import ctypes
 
 
-def check_cuda():
+def check_cuda() -> bool:
     """
-    Check if CUDA GPUs are available without importing torch or tensorflow
+    Check if CUDA GPUs are available without importing torch or tensorflow.
 
-    Returns
-    -------
-    bool
-        True if a CUDA GPU is available, False otherwise
+    Returns:
+        bool: True if a CUDA GPU is available, False otherwise.
     """
     try:
         ctypes.cdll.LoadLibrary("libcuda.so")
