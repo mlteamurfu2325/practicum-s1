c121f2ef7e01618e5271a6db6df7cb2bd199bf15
user1 <ikhardin000@gmail.com>
2024-03-24 21:08:54 +0500

    Refactor deploy/download_faster_whisper_models.py

--- deploy/download_faster_whisper_models.py
+++ deploy/download_faster_whisper_models.py
@@ -1,17 +1,22 @@
 """Module for downloading the Faster-Whisper models."""
 
+import os
 from pathlib import Path
-
 from faster_whisper import download_model
 
 
-DIR = str(Path.cwd()) + "/models/faster-whisper/"
+DIR = os.path.join(str(Path.cwd()), "models", "faster-whisper")
 MODELS = ["medium", "large-v3"]
 
-for model in MODELS:
-    try:
-        download_model(model, output_dir=f"{DIR}/{model}/")
-    except Exception as error:
-        print(f"An exception occured: {error}")
 
-print("Done!")
+def download_models():
+    for model in MODELS:
+        try:
+            download_model(model, output_dir = os.path.join(DIR, model))
+        except Exception as err:
+            print(f"An exception occured while downloading model {model}: {err}")
+        print("Model download!")
+
+
+if __name__ == "__main__":
+    download_models()
